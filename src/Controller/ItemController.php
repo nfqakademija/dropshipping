fa61c1ac6fb83a39b7e91c6f0cb3371fc575016f
skuziokas <skuziokas@yahoo.com>
2019-04-29 03:31:05 +0300

    Item CRUD

---
+++ src/Controller/ItemController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Item;
+use App\Form\ItemType;
+use App\Repository\ItemRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/item")
+ */
+class ItemController extends AbstractController
+{
+    /**
+     * @Route("/", name="item_index", methods={"GET"})
+     */
+    public function index(ItemRepository $itemRepository): Response
+    {
+        return $this->render('item/index.html.twig', [
+            'items' => $itemRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="item_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $item = new Item();
+        $form = $this->createForm(ItemType::class, $item);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($item);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('item_index');
+        }
+
+        return $this->render('item/new.html.twig', [
+            'item' => $item,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="item_show", methods={"GET"})
+     */
+    public function show(Item $item): Response
+    {
+        return $this->render('item/show.html.twig', [
+            'item' => $item,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="item_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Item $item): Response
+    {
+        $form = $this->createForm(ItemType::class, $item);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('item_index', [
+                'id' => $item->getId(),
+            ]);
+        }
+
+        return $this->render('item/edit.html.twig', [
+            'item' => $item,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="item_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Item $item): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$item->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($item);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('item_index');
+    }
+}

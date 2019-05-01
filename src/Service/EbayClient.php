5582894fcda7850e16411fe413b1be25dbf74e99
Mantas Pudžiavis <mpudziavis@gmail.com>
2019-05-01 15:48:26 -0400

    Added ebay api parameters and layouts to other pages

--- src/Service/EbayClient.php
+++ src/Service/EbayClient.php
@@ -34,7 +34,7 @@ class EbayClient
     public function __construct($sandbox)
     {
         $this->ruName = $sandbox[3]['ruName'];
-        $this->sessionId = $this->getSessionId($sandbox);
+//        $this->sessionId = $this->getSessionId($sandbox);
 //        $this->userId = $userId;
     }
 
@@ -59,34 +59,6 @@ class EbayClient
 //
 //        $sesId = urlencode($_SESSION['ebSession']);
 
-        $service = new TradServ\TradingService([
-            'credentials' => $sandbox[0]['credentials'],
-            'sandbox' => true,
-            'siteId'      => Constants\SiteIds::US
-        ]);
-
-        $getsessionid = $service->getSessionID(
-            new TradTypes\GetSessionIDRequestType([
-                'RuName' => 'Mantas_Pudziavi-MantasPu-codezi-asshpnqfd'
-            ])
-        );
-        $_SESSION["sessionid"] = $getsessionid->SessionID;
-
-        return $this->loginToApplicationUrl();
-    }
-
-    public function loginToApplicationUrl()
-    {
-        return 'https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&RuName=Mantas_Pudziavi-MantasPu-codezi-asshpnqfd&SessID='.urlencode($_SESSION['sessionid']);
-    }
-
-    public function oauthUrl()
-    {
-        return 'https://auth.sandbox.ebay.com/oauth2/authorize?client_id=MantasPu-codezipk-SBX-555b8f5d2-321e0c95&response_type=code&redirect_uri=Mantas_Pudziavi-MantasPu-codezi-asshpnqfd&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly';
-    }
-
-    public function getUserToken($sandbox)
-    {
 //        $service = new TradServ\TradingService([
 //            'credentials' => $sandbox[0]['credentials'],
 //            'sandbox' => true,
@@ -95,30 +67,58 @@ class EbayClient
 //
 //        $getsessionid = $service->getSessionID(
 //            new TradTypes\GetSessionIDRequestType([
-//                'RuName' => 'Mantas_Pudziavi-MantasPu-codezi-bovmcywta'
+//                'RuName' => 'Mantas_Pudziavi-MantasPu-codezi-asshpnqfd'
 //            ])
 //        );
 //        $_SESSION["sessionid"] = $getsessionid->SessionID;
 //
-        $theID = $_SESSION["sessionid"];
+//        return $this->loginToApplicationUrl();
+    }
 
-        $requestXmlBody = '<?xml version="1.0" encoding="utf-8" ?>';
-        $requestXmlBody .= '<FetchTokenRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
-        $requestXmlBody .= '<WarningLevel>High</WarningLevel>';
-        $requestXmlBody .= "<SessionID>".$theID."</SessionID>";
-        $requestXmlBody .= "<Version>1031</Version>";
-        $requestXmlBody .= '</FetchTokenRequest>';
-        $session = new eBaySession($sandbox);
-        //send the request and get response
-        $responseXml = $session->sendHttpRequest($requestXmlBody, 'FetchToken');
+//    public function loginToApplicationUrl()
+//    {
+//        return 'https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&RuName=Mantas_Pudziavi-MantasPu-codezi-asshpnqfd&SessID='.urlencode($_SESSION['sessionid']);
+//    }
 
-        if(stristr($responseXml, 'HTTP 404') || $responseXml == '')
-            die('<P>Error sending request');
+    public function oauthUrl()
+    {
+        return 'https://auth.sandbox.ebay.com/oauth2/authorize?client_id=MantasPu-codezipk-SBX-555b8f5d2-321e0c95&response_type=code&redirect_uri=Mantas_Pudziavi-MantasPu-codezi-asshpnqfd&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly';
+    }
 
-        $resp = simplexml_load_string($responseXml);
+    public function getUserToken($sandbox)
+    {
+////        $service = new TradServ\TradingService([
+////            'credentials' => $sandbox[0]['credentials'],
+////            'sandbox' => true,
+////            'siteId'      => Constants\SiteIds::US
+////        ]);
+////
+////        $getsessionid = $service->getSessionID(
+////            new TradTypes\GetSessionIDRequestType([
+////                'RuName' => 'Mantas_Pudziavi-MantasPu-codezi-bovmcywta'
+////            ])
+////        );
+////        $_SESSION["sessionid"] = $getsessionid->SessionID;
+////
+//        $theID = $_SESSION["sessionid"];
 //
+/*        $requestXmlBody = '<?xml version="1.0" encoding="utf-8" ?>';*/
+//        $requestXmlBody .= '<FetchTokenRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+//        $requestXmlBody .= '<WarningLevel>High</WarningLevel>';
+//        $requestXmlBody .= "<SessionID>".$theID."</SessionID>";
+//        $requestXmlBody .= "<Version>1031</Version>";
+//        $requestXmlBody .= '</FetchTokenRequest>';
+//        $session = new eBaySession($sandbox);
+//        //send the request and get response
+//        $responseXml = $session->sendHttpRequest($requestXmlBody, 'FetchToken');
 //
-        return $resp;
+//        if(stristr($responseXml, 'HTTP 404') || $responseXml == '')
+//            die('<P>Error sending request');
+//
+//        $resp = simplexml_load_string($responseXml);
+////
+////
+//        return $resp;
 
     }
 

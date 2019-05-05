7ae648fdbacf7e4fa8446248204d6d4a1f0d9f56
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-05 19:39:22 +0300

    Added AliExpress library and AliExpressController

---
+++ src/Controller/AliExpressController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use AliseeksApi\Configuration;
+use AliseeksApi\Api\ProductsApi;
+use AliseeksApi\Model\ProductRequest;
+use AliseeksApi\Model\ProductDetailsRequest;
+use GuzzleHttp\Client;
+use AliseeksApi\ApiException;
+
+class AliExpressController extends AbstractController
+{
+    /**
+     * @Route("/ali/express", name="ali_express")
+     */
+    public function index()
+    {
+        // Configure API key authorization: ApiKeyAuth
+        $config = Configuration::getDefaultConfiguration()->setApiKey('X-API-CLIENT-ID', 'TEIEQFRZKOYVNPUL');
+
+
+        $apiInstance = new ProductsApi(
+        // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+        // This is optional, `GuzzleHttp\Client` will be used as default.
+            new Client(),
+            $config
+        );
+        $product_request = new ProductDetailsRequest(); // \AliseeksApi\Model\ProductRequest | The request body of get product
+        $product_request->setProductId('32826897725');
+
+        try {
+            $result = $apiInstance->getProduct($product_request);
+            print_r($result);
+        } catch (ApiException $e) {
+            echo 'Exception when calling ProductsApi->getProduct: ', $e->getMessage(), PHP_EOL;
+        }
+
+
+
+
+
+
+
+        return $this->render('ali_express/index.html.twig', [
+            'controller_name' => 'AliExpressController',
+        ]);
+    }
+}

793337f9a7a78398aca81411769aa078590a5b5b
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-05 23:41:55 +0300

    Updated ItemImport form, included form in dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ItemImportType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 
@@ -13,13 +15,16 @@ class DashboardController extends AbstractController
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $form = $this->createForm(ItemImportType::class);
+        $form->handleRequest($request);
 
         $user = $this->get('security.token_storage')->getToken()->getUser()->getFirstName();
 
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => $user,
+            'controller_name' => $user, 'itemInputForm' => $form->createView()
         ]);
     }
 }

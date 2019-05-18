caa5dec2f388d8017a04a40e4e45a6de32bd5286
skuziokas <skuziokas@yahoo.com>
2019-05-18 11:17:52 +0300

    Updated AmazonDataSaver

--- src/Amazon/AmazonDataSaver.php
+++ src/Amazon/AmazonDataSaver.php
@@ -4,16 +4,15 @@
 namespace App\Amazon;
 
 
-/*use AliseeksApi\Model\ProductDetail;
-use AliseeksApi\Model\ProductHtmlDescription;
-use App\Entity\AliExpressCategory;
-use App\Entity\AliExpressItem;
-use App\Entity\Image;
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Security;*/
-
 class AmazonDataSaver
 {
+    
+    /**
+     * @param $product
+     */
+    public function storeProduct($product)
+    {
+        
+    }
     
 }

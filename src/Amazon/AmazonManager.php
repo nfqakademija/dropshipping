1def87688c1479780297a4f7964cd3ea7cad0dce
skuziokas <skuziokas@yahoo.com>
2019-05-18 10:57:30 +0300

    Updated AmazonManager

--- src/Amazon/AmazonManager.php
+++ src/Amazon/AmazonManager.php
@@ -6,4 +6,34 @@ namespace App\Amazon;
 
 class AmazonManager{
     
+    private $provider;
+
+    private $dataSaver;
+
+    private $productId;
+
+    private $product;
+
+    public function __construct(AmazonProvider $provider, AmazonDataSaver $dataSaver)
+    {
+        $this->provider = $provider;
+        $this->dataSaver = $dataSaver;
+    }
+
+    public function addProduct(array $product)
+    {
+        $this->setProductId($product);
+
+        $this->product = $this->provider->getProductData($this->productId);
+
+        $this->dataSaver->storeProduct($this->product);
+
+    }
+
+    public function  setProductId(array $product)
+    {
+        $product;
+        $this->productId = "B07JHJ56PR";
+    }
+    
 }

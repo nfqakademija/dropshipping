b76ab65496d5ffc9eac38651fdf6ae431e910bd0
skuziokas <skuziokas@yahoo.com>
2019-05-18 11:10:23 +0300

    Updated AmazonProvider

--- src/Amazon/AmazonProvider.php
+++ src/Amazon/AmazonProvider.php
@@ -6,4 +6,17 @@ namespace App\Amazon;
 
 class AmazonProvider{
     
+    private $productData;
+    
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function getProductData($productId)
+    {
+        return $this->productData;
+    }
+    
 }

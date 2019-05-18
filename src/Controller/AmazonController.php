edc632524ab4c3b5fd31336deb380a66381a79a2
skuziokas <skuziokas@yahoo.com>
2019-05-18 09:55:48 +0300

    Updated AmazonController

--- src/Controller/AmazonController.php
+++ src/Controller/AmazonController.php
@@ -10,5 +10,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AmazonController extends AbstractController
 {
-    
+    /**
+     * @param array $data
+     * @param AmazonManager $amazonManager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function importProduct(array $data, AmazonManager $amazonManager)
+    {
+        $amazonManager->addProduct($data);
+
+        return $this->redirectToRoute('dashboard');
+    }
 }

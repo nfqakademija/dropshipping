013c50c7a8a117c21dc8c2681a80a343c8883be8
Giedrius Sk <skuziokas@yahoo.com>
2019-06-01 14:05:58 +0300

    Created AmazonToEbayManager

---
+++ src/Service/Amazon/AmazonToEbay/AmazonToEbayManager.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Service\Amazon\AmazonToEbay;
+
+
+use \DTS\eBaySDK\Trading\Enums;
+use \DTS\eBaySDK\Trading\Services;
+use \DTS\eBaySDK\Trading\Types;
+
+class AmazonToEbayManager
+{
+    private $productData;
+
+    
+    
+
+    /**
+     * @param array $product
+     */
+    public function addProductToEbay(array $product)
+    {
+      
+
+    }
+
+}

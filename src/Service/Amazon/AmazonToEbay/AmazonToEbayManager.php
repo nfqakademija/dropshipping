7c368ba3e771e25555510eeb26e61d8ccf46b722
Giedrius Sk <skuziokas@yahoo.com>
2019-06-03 08:46:28 +0300

    Updated AmazonToEbayManager

--- src/Service/Amazon/AmazonToEbay/AmazonToEbayManager.php
+++ src/Service/Amazon/AmazonToEbay/AmazonToEbayManager.php
@@ -1,18 +1,43 @@
 <?php
 
-
 namespace App\Service\Amazon\AmazonToEbay;
-
 
 use \DTS\eBaySDK\Trading\Enums;
 use \DTS\eBaySDK\Trading\Services;
 use \DTS\eBaySDK\Trading\Types;
+use App\AliExpressToEbay\EbayItem;
 
 class AmazonToEbayManager
 {
     private $productData;
+    
+    /**
+     * @var Types\AddFixedPriceItemRequestType $ebayRequest
+     */
+    private $ebayRequest;
+
+    /**
+     * @var Services\TradingService $ebayService
+     */
+    private $ebayService;
+
+    /**
+     * @var AddedEbayItemDataSaver
+     */
+    private $addedEbayItemDataSaver;
 
     
+    
+    public function __construct(
+        EbayRequest $ebayRequest,
+        EbayService $ebayService,
+        AddedEbayItemDataSaver $addedEbayItemDataSaver
+    )
+    {
+        $this->ebayRequest = $ebayRequest->getRequest();
+        $this->ebayService = $ebayService;
+        $this->addedEbayItemDataSaver = $addedEbayItemDataSaver;
+    }
     
 
     /**
@@ -20,6 +45,21 @@ class AmazonToEbayManager
      */
     public function addProductToEbay(array $product)
     {
+        $this->ebayService->setService($product['shopCountry']);
+        $this->ebayService = $this->ebayService->getService();
+
+        $ebayItem = new EbayItem();
+        $ebayItem->addPropertiesToItem($product);
+        $item = $ebayItem->getItem();
+        $this->ebayRequest->Item = $item;
+
+        try{
+            $response = $this->ebayService->addFixedPriceItem($this->ebayRequest);
+        } catch (\Exception $e) {
+            $e->getMessage();
+        }
+
+        $this->addedEbayItemDataSaver->saveEbayItem($product['id'], $response->ItemID, 'amazon');
       
 
     }

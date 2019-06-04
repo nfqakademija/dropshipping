38b3ff7087faadb65410bbf562a9f091f476c38b
KosAugu <kostas.augustinavicius@yahoo.com>
2019-06-04 14:18:01 +0300

    Updated Aliexpress to ebay classes

--- src/AliExpressToEbay/EbayItem.php
+++ src/AliExpressToEbay/EbayItem.php
@@ -5,7 +5,6 @@ namespace App\AliExpressToEbay;
 
 use \DTS\eBaySDK\Trading\Types;
 use \DTS\eBaySDK\Trading\Enums;
-use \DTS\eBaySDK\Trading\Services;
 
 class EbayItem
 {
@@ -16,7 +15,7 @@ class EbayItem
 
     public function getItem()
     {
-       return $this->item;
+        return $this->item;
     }
 
     public function addPropertiesToItem(array $data)
@@ -24,9 +23,9 @@ class EbayItem
         $this->item = new Types\ItemType();
         $this->item->ListingType = Enums\ListingTypeCodeType::C_FIXED_PRICE_ITEM;
 
-        $this->item->Quantity = (int) $data['stock'];
+        $this->item->Quantity = (int)$data['stock'];
         $this->item->ListingDuration = Enums\ListingDurationCodeType::C_GTC;
-        $this->item->StartPrice = new Types\AmountType(['value' => (double) $data['price']]);
+        $this->item->StartPrice = new Types\AmountType(['value' => (double)$data['price']]);
 
 
         $this->item->Title = $data['title'];
@@ -42,20 +41,27 @@ class EbayItem
         ];
         $this->item->PayPalEmailAddress = 'example@example.com';
         $this->item->DispatchTimeMax = 1;
+        $this->item->PrimaryCategory = new Types\CategoryType();
+        $this->item->PrimaryCategory->CategoryID = $data['category'];
+
         $this->addCurrency($data);
-        $this->addCategory();
         $this->addLocation();
-        $this->addShipping();
+        $this->addShipping($data);
     }
 
-    public function addShipping()
+    public function addShipping(array $data)
     {
         $this->item->ShippingDetails = new Types\ShippingDetailsType();
         $this->item->ShippingDetails->ShippingType = Enums\ShippingTypeCodeType::C_FLAT;
 
         $shippingService = new Types\ShippingServiceOptionsType();
         $shippingService->ShippingServicePriority = 1;
-        $shippingService->ShippingService = 'DE_DeutschePostBrief';
+        if ($data['shopCountry'] == "DE") {
+            $shippingService->ShippingService = 'DE_DeutschePostBrief';
+        } elseif ($data['shopCountry'] == "GB") {
+            $shippingService->ShippingService = 'UK_RoyalMailFirstClassStandard';
+        }
+
         $shippingService->ShippingServiceCost = new Types\AmountType(['value' => 2.00]);
         $shippingService->ShippingServiceAdditionalCost = new Types\AmountType(['value' => 1.00]);
         $this->item->ShippingDetails->ShippingServiceOptions[] = $shippingService;
@@ -64,22 +70,14 @@ class EbayItem
 
     public function addLocation()
     {
-        //ToDo add location from user settings
         $this->item->Country = "US";
         $this->item->Location = "New City";
         $this->item->PostalCode = "10956";
     }
 
-    public function addCategory()
-    {
-        //ToDo add category suggestion or search list for categories
-        $this->item->PrimaryCategory = new Types\CategoryType();
-        $this->item->PrimaryCategory->CategoryID = '29792';
-    }
-
     public function addCurrency(array $data)
     {
-        if($data['shopCountry'] == "DE") {
+        if ($data['shopCountry'] == "DE") {
             $this->item->Currency = "EUR";
         } elseif ($data['shopCountry'] == "GB") {
             $this->item->Currency = "GBP";
